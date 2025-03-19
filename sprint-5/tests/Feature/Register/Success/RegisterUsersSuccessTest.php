5b79ab79fd37dec7fd0a47ce93333f150f77b85d
Ramón Martos Oliver <soughtsingularity@gmail.com>
2025-03-19 18:45:12 +0100

    The route must be up

---
+++ sprint-5/tests/Feature/Register/Success/RegisterUsersSuccessTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature\Register\Success;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterUsersSuccessTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_register_and_receive_token()
+    {
+        //$this->withoutExceptionHandling();
+
+        $response = $this->postJson('/api/register', [
+            'username' => 'example',
+            'email' => 'example@gmail.com',
+            'password' => '12345678',
+            'password_confirmation' => '12345678',
+        ]);
+
+        $response->assertStatus(201);
+    }
+}
