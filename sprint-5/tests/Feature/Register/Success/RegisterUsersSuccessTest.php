aac8414189a17000c7df434cd158ad1780ad8e6a
Ramón Martos Oliver <soughtsingularity@gmail.com>
2025-03-20 21:53:41 +0100

    user is saved in database

--- sprint-5/tests/Feature/Register/Success/RegisterUsersSuccessTest.php
+++ sprint-5/tests/Feature/Register/Success/RegisterUsersSuccessTest.php
@@ -5,12 +5,12 @@ namespace Tests\Feature\Register\Success;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class RegisterUsersSuccessTest extends TestCase
 {
     public function test_user_can_register()
     {
-        $this->withoutExceptionHandling();
 
         $response = $this->postJson('/api/register', [
             'username' => 'example',
@@ -21,7 +21,30 @@ class RegisterUsersSuccessTest extends TestCase
 
         $response->assertStatus(201)
             ->assertJsonStructure(['message', 'user']);
-        
     }
+
+    public function test_new_user_is_saved_in_database()
+    {
+        $userData = [
+            'username' => 'example',
+            'email' => 'example2@gmail.com',
+            'password' => '12345678!',
+            'password_confirmation' => '12345678!',
+        ];
+
+        $response = $this->postJson('/api/register', $userData);
+
+        $response->assertStatus(201)
+            ->assertJsonStructure(['message', 'user']);
+
+            $this->assertDatabaseHas('users', [
+                'email' => $userData['email'],
+                'username' => $userData['username'],
+            ]);
+
+            $user = User::where('email', $userData['email'])->first();
+            $this->assertTrue(app('hash')->check($userData['password'], $user->password));
+    }
+
 }
 
