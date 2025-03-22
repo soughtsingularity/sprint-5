060776c6a4181c54aa5b5de48e76055b3569aff0
Ramón Martos Oliver <soughtsingularity@gmail.com>
2025-03-22 10:21:06 +0100

    email_is_required

---
+++ sprint-5/app/Http/Requests/LoginUserRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|unique:users',
+        ];
+    }
+}
---
+++ sprint-5/tests/Unit/LoginUserValidationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\LoginUserRequest;
+
+class LoginUserValidationTest extends TestCase
+{
+    public function test_email_is_required()
+    {
+        $data = [
+            'email' => '',
+            'password' => '12345678!',
+        ];
+
+        $request = new LoginUserRequest();
+        $request->merge($data);
+
+        $this->expectException(ValidationException::class);
+
+        $validator = Validator::make($request->all(), $request->rules());
+        if($validator->fails()){
+            throw new ValidationException($validator);
+        }    
+    }
+}
