7ab2c7a940f242a599ce9386f8b62ca41d082656
Ramón Martos Oliver <soughtsingularity@gmail.com>
2025-03-24 14:00:58 +0100

    user is saved in database

--- sprint-5/tests/Feature/Register/Success/LoginUsersSucccessTest.php
+++ sprint-5/tests/Feature/Register/Success/LoginUsersSucccessTest.php
@@ -50,4 +50,30 @@ class LoginUsersSucccessTest extends ApiTestCase
                 'token',
         ]);
     }
+
+    public function test_user_can_login_with_token_and_be_Saved_in_database()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create([
+            'email' => 'example@example.com',
+            'password' => bcrypt('password123!'),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'example@example.com',
+            'password' => 'password123!',
+        ]);
+
+        $response->assertStatus(200)
+                ->assertJsonStructure([
+                'message',
+                'user',
+                'token',
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'example@example.com'
+        ]);
+    }
 }
