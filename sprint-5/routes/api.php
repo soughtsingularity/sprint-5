1585d80c1579bfd1c4e90cdae2419bae3386a2ae
Ramón Martos Oliver <soughtsingularity@gmail.com>
2025-03-25 17:34:52 +0100

    a user can delete an account

--- sprint-5/routes/api.php
+++ sprint-5/routes/api.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [RegisterController::class, 'register']);
 
 Route::post('login', [LoginController::class, 'login']);
+
+Route::middleware(['auth:api', 'role:user'])
+    ->delete('/user/{user}', [UserController::class, 'destroy']);
