ff6a1f58d976c522fb0500a82fb3cf0120bd26ae
Ramón Martos Oliver <soughtsingularity@gmail.com>
2025-03-31 11:21:01 +0200

    user cannot update course

---
+++ sprint-5/tests/Feature/Course/Validation/UpdateCourseFailTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature\Course\Validation;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\ApiTestCase;
+use App\Models\User;
+
+class UpdateCourseFailTest extends ApiTestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_cannot_update_course()
+    {
+        //$this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+        $user->assignRole('user');
+
+        $oldCourseData = [
+            'title' => 'OldCourse',
+            'description' => 'OldCourseDescription',
+            'content' => [
+                [
+                    'title' => 'oldChapter',
+                    'description' => 'oldDescription',
+                    'videos' => [
+                        [
+                            'title' => 'oldVideo',
+                            'description' => 'oldVideo1Description',
+                            'url' => 'https://www.youtube.com/watch?v=video1'
+                        ],
+                    ]
+                ],
+            ]
+        ];
+
+
+        $course = $user->courses()->create($oldCourseData);
+
+        $updatedCourseData = [
+            'title' => 'UpdatedCourse',
+            'description' => 'UpdatedCourseDescription',
+            'content' => [
+                [
+                    'title' => 'UpdatedChapter',
+                    'description' => 'UpdatedDescription',
+                    'videos' => [
+                        [
+                            'title' => 'UpdatedVideo',
+                            'description' => 'UpdatedVideo1Description',
+                            'url' => 'https://www.youtube.com/watch?v=video1'
+                        ],
+                    ]
+                ],
+            ]
+        ];
+
+        $token = $user->createToken('authToken')->accessToken;
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('PUT', '/api/courses/' . $course->id, $updatedCourseData);
+
+        $response->assertStatus(403);
+
+    }
+
+}
