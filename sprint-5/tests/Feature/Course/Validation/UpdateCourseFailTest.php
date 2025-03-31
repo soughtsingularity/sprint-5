4e6c00114250d033388185bcd6fd137f519e0278
Ramón Martos Oliver <soughtsingularity@gmail.com>
2025-03-31 11:23:56 +0200

    admin cannot update course with invalid token

--- sprint-5/tests/Feature/Course/Validation/UpdateCourseFailTest.php
+++ sprint-5/tests/Feature/Course/Validation/UpdateCourseFailTest.php
@@ -117,4 +117,59 @@ class UpdateCourseFailTest extends ApiTestCase
             ]);
     }
 
+    public function test_admin_cannot_update_course_with_invalid_token()
+    {
+        //$this->withoutExceptionHandling();
+
+        $admin = User::factory()->create();
+        $admin->assignRole('admin');
+        $token = 'invalid_token';
+
+        $oldCourseData = [
+            'title' => 'OldCourse',
+            'description' => 'OldCourseDescription',
+            'content' => [
+                [
+                    'title' => 'oldChapter',
+                    'description' => 'oldDescription',
+                    'videos' => [
+                        [
+                            'title' => 'oldVideo',
+                            'description' => 'oldVideo1Description',
+                            'url' => 'https://www.youtube.com/watch?v=video1'
+                        ],
+                    ]
+                ],
+            ]
+        ];
+
+        $course = $admin->courses()->create($oldCourseData);
+
+        $updatedCourseData = [
+            'title' => 'UpdatedCourse',
+            'description' => 'UpdatedCourseDescription',
+            'content' => [
+                [
+                    'title' => 'UpdatedChapter',
+                    'description' => 'UpdatedDescription',
+                    'videos' => [
+                        [
+                            'title' => 'UpdatedVideo',
+                            'description' => 'UpdatedVideo1Description',
+                            'url' => 'https://www.youtube.com/watch?v=video1'
+                        ],
+                    ]
+                ],
+            ]
+        ];
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->json('PUT', '/api/courses/' . $course->id, $updatedCourseData);
+        
+        $response->assertStatus(401)
+            ->assertJson([
+                'message' => 'Unauthenticated.',
+            ]);
+    }
+
 }
