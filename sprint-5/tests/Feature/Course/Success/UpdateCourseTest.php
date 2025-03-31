4e84c679a5e79388c4155dfd4f979be698982ba3
Ramón Martos Oliver <soughtsingularity@gmail.com>
2025-03-31 10:24:41 +0200

    Test for happy path, route and permission ok

---
+++ sprint-5/tests/Feature/Course/Success/UpdateCourseTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Tests\Feature\Course\Success;
+
+use App\Models\User;
+use Tests\ApiTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UpdateCourseTest extends ApiTestCase
+{
+    use RefreshDatabase;
+
+    public function test_admin_can_update_a_course()
+    {
+        $this->withoutExceptionHandling();
+
+        $admin = User::factory()->create();
+        $admin->assignRole('admin');
+
+        $oldCourseData = [
+            'title' => 'OldCourse',
+            'description' => 'OldCourseDescription',
+            'content' => [
+                [
+                    'title' => 'oldChapter',
+                    'description' => 'oldDescription',
+                    'videos' => [
+                        [
+                            'title' => 'oldVideo',
+                            'description' => 'oldVideo1Description',
+                            'url' => 'https://www.youtube.com/watch?v=video1'
+                        ],
+                    ]
+                ],
+            ]
+        ];
+
+
+        $course = $admin->courses()->create($oldCourseData);
+
+        $updatedCourseData = [
+            'title' => 'UpdatedCourse',
+            'description' => 'UpdatedCourseDescription',
+            'content' => [
+                [
+                    'title' => 'UpdatedChapter',
+                    'description' => 'UpdatedDescription',
+                    'videos' => [
+                        [
+                            'title' => 'UpdatedVideo',
+                            'description' => 'UpdatedVideo1Description',
+                            'url' => 'https://www.youtube.com/watch?v=video1'
+                        ],
+                    ]
+                ],
+            ]
+        ];
+
+        $token = $admin->createToken('authToken')->accessToken;
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->putJson('/api/courses/' . $course->id, $updatedCourseData);
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'title' => $updatedCourseData['title'],
+                'description' => $updatedCourseData['description'],
+            ]);
+
+
+    }
+}
