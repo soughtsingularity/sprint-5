3a94746212f07f937e43ace60c80f94bb254bd7f
Ramón Martos Oliver <soughtsingularity@gmail.com>
2025-03-31 13:18:31 +0200

    user cannot complete chapter with invalid token

--- sprint-5/tests/Feature/Progress/Validation/CompletedChapterFailTest.php
+++ sprint-5/tests/Feature/Progress/Validation/CompletedChapterFailTest.php
@@ -50,4 +50,45 @@ class CompletedChapterFailTest extends ApiTestCase
             'message' => 'Unauthenticated.',
         ]);
     }
+
+    public function test_complete_chapter_with_invalid_token()
+    {
+        //$this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+        $user->assignRole('user');
+        $token = 'Invalid token';
+
+        $course = Course::factory()->create([
+            'title' => 'Test Course',
+            'description' => 'This is a test course',
+            'content' => json_encode([
+                [
+                    'title' => 'Capítulo 1',
+                    'description' => 'Intro',
+                    'videos' => [
+                        [
+                            'title' => 'Video 1',
+                            'description' => 'Desc video 1',
+                            'url' => 'https://youtube.com/watch?v=abc123'
+                        ]
+                    ]
+                ]
+            ]),
+        ]);
+
+        $user->courses()->attach($course->id);
+
+        $courseId = $course->id;
+        $chapterIndex = 0;
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->postJson("/api/courses/{$courseId}/chapters/{$chapterIndex}/complete");
+
+        $response->assertStatus(401);
+        $response->assertJson([
+            'message' => 'Unauthenticated.',
+        ]);
+    }
 }
